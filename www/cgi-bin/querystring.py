53c518cf4b515cebf96e4b0c7d04960e3cbe1f0b
Mohamed Amine Maila <aminemaila18@gmail.com>
2025-03-07 05:06:05 +0000

    added final touches

--- www/cgi-bin/querystring.py
+++ www/cgi-bin/querystring.py
@@ -4,14 +4,11 @@ import html
 
 print("Content-Type: text/html\r\n\r\n", end="")
 
-# Get query parameters
 form = cgi.FieldStorage()
 name = form.getvalue('name')
 
 if name:
-    # Escape user input to prevent XSS attacks
-    safe_name = html.escape(name)
-    print(f"<h1>Hello {safe_name}!</h1>")
+    print(f"<h1>Hello {name}!</h1>")
 else:
     print("<h1>Hello stranger!</h1>")
 
